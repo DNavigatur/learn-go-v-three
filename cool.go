41a529b4d8690c6e5c1b9d16a043c224ed6a914e
DNavigatur <98771560+DNavigatur@users.noreply.github.com>
2023-08-16 10:26:36 +0100

    commit cool

---
+++ cool.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	puppy "github.com/DNavigatur/goPuppy"
+)
+
+func main() {
+
+	fmt.Printf("Hello world\n")
+
+	fmt.Println(puppy.Bark())
+
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module myModule
+
+go 1.20
+
+require github.com/DNavigatur/goPuppy v0.0.0-20230815172400-d9c6809f3e56
